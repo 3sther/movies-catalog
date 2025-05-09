d7e3f5a66fde5cdce94938051e5cc096d74454b0
3sther <iwebfunctional@gmail.com>
2025-05-09 21:57:41 +0600

    added welcome msg to root

--- app/main.py
+++ app/main.py
@@ -1,5 +1,19 @@
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    Request,
+)
 
 app = FastAPI(
     title="Movies Catalog",
 )
+
+
+@app.get("/")
+def root_informer(
+    # Get incoming url obj to adjust
+    request: Request,
+):
+    path_to_docs = request.url.replace(
+        path="/docs",
+    )
+    return {"Our catalog is currently empty, please go to our docs": str(path_to_docs)}
